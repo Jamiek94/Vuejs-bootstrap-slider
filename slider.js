c0fb139f2c903b4819e1058fa9e1bc6499d42d6a
Jamie <Jamie.knoef@gmail.com>
2016-07-26 19:42:41 +0200

    added vue.js slider component

---
+++ slider.js
@@ -0,0 +1,68 @@
+module.exports = {
+    template: "<input id='{{name}}' type='text'/>",
+    props: {
+        min: {
+            type: Number,
+            required: true
+        },
+        max: {
+            type: Number,
+            required: true
+        },
+        value: {
+            required: true
+        },
+        ticks: {
+            type: Array,
+            required: false
+
+        },
+        ticksLabels: {
+            type: Array,
+            required: false
+
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        step : {
+            type : Number,
+            required : false,
+            default : 1
+        },
+        tooltip : {
+            type : Boolean,
+            required : false,
+            default : true
+        },
+        changeEventName : {
+            type : String,
+            required : false,
+            default : "slide-change"
+        }
+    },
+    ready: function () {
+        var self = this;
+
+        Vue.nextTick(function () {
+            var sliderElement = $("#" + self.name);
+
+            var options = {
+                value  : self.value,
+                ticks: self.ticks,
+                ticks_labels: self.ticksLabels,
+                min: self.min,
+                max: self.max,
+                step: self.step,
+                tooltip: self.tooltip ? "show" : 'hide'
+            };
+
+            sliderElement.slider(options);
+
+            sliderElement.on("slide", function (event) {
+                self.$dispatch(self.changeEventName, event.value)
+            });
+        });
+    }
+};
